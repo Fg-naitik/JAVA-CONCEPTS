ea4a20b07895fa0ca228009a5202490c6d66b4d4
Fg-naitik <naitikbhandari63@gmail.com>
2025-10-05 03:15:35 +0000

    no message

---
+++ Student.java
@@ -0,0 +1,27 @@
+/*Create a Student class with the following attributes:
+•	name (String)
+•	marks (int array of size 5)
+Add the following methods:
+•	inputDetails() – to take user input for the student's name and marks in 5 subjects
+•	calculateAverage() – returns the average marks
+•	displayGrade() – displays grade based on average:
+o	>=90 → A
+o	80-89 → B
+o	70-79 → C
+o	<70 → D                                                                       
+----------------------------------------------code here----------------------------------------------------*/
+class Student{
+    string name;
+    int marks[5];
+
+    void inputDetails(){        // code to take user input for name and marks
+        system.out.println("the name the student is:");
+    }
+    float calculateAverage(){     // code to calculate and return average marks
+        
+    }
+    void displayGrade(){
+        // code to display grade based on average marks
+    }
+}
+
